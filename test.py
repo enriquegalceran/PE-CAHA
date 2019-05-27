ad72609a94b63f092dc002c30205de23a455fb21
Enrique Galceran <enrique@galceran.net>
2019-05-27 21:32:04 +0200

    Adaptado para archivos fits. Falta por mirar cuando se tiene que activar y juntar esto con el resto del código

--- test.py
+++ test.py
@@ -1,16 +1,42 @@
 import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
-#https://stackoverflow.com/questions/44865023/circular-masking-an-image-in-python-using-numpy-arrays
-lado = 2048
-a = np.arange(lado**2).reshape(lado,lado)
-x1 = 1224
-x2 = 1624
-y1 = 1
-y2 = 200
-b = np.copy(a[y1:y2, x1:x2])
-print(a.shape)
-print(b.shape)
+from Salida_limpia import mostrarresultados, stdrobusta
+from astropy.io import fits
+import IMGPlot as ImP
+
+
+def deshacer_tupla_coord(tupla):
+    return tupla[0], tupla[1], tupla[2], tupla[3]
+
+
+def sacar_coordenadas_ccd(imagen_, mypath_=False):
+    if mypath_:
+        str_ccdsec = fits.open(mypath_ + imagen_)[0].header['CCDSEC']
+        longitud = len(str_ccdsec)
+    else:
+        str_ccdsec = imagen_
+        longitud = len(str_ccdsec)+1
+    for x in range(1, longitud):
+        if str_ccdsec[x] == ',':
+            coma = x
+            break
+    for x in range(coma + 1, longitud):
+        if str_ccdsec[x] == ':':
+            puntos = x
+            break
+    for x in range(puntos + 1, longitud):
+        if str_ccdsec[x] == ',':
+            coma2 = x
+            break
+
+    x1 = int(str_ccdsec[1:coma])
+    y1 = int(str_ccdsec[coma + 1: puntos])
+    x2 = int(str_ccdsec[puntos + 1: coma2])
+    y2 = int(str_ccdsec[coma2 + 1: -1])
+    tupla_salida = (x1, x2, y1, y2)
+    return tupla_salida
+
 
 def create_circular_mask(h, w, center=None, radius=None):
 
@@ -25,41 +51,49 @@ def create_circular_mask(h, w, center=None, radius=None):
     mask = dist_from_center <= radius
     return mask
 
-h, w = b.shape[:2]
+#https://stackoverflow.com/questions/44865023/circular-masking-an-image-in-python-using-numpy-arrays
+lado = 2048
+a = np.arange(lado**2).reshape(lado,lado)
+x1 = 1224
+x2 = 1624
+y1 = 1
+y2 = 200
+b = np.copy(a[y1:y2, x1:x2])
 
-center_real = [1024,1024]
 
+image_file = 'imagencopiada.fits'
+ccdsec = fits.open(image_file)[0].header['CCDSEC']
+image_data = fits.getdata(image_file, ext=0)
+coordenadas_ = sacar_coordenadas_ccd(ccdsec)
+x1, x2, y1, y2 = deshacer_tupla_coord(coordenadas_)
+
+
+h, w = image_data.shape[:2]
+center_real = [1075, 1040]
 center = [center_real[0]-x1, center_real[1]-y1]
-# Esto habra que cambiarlo, porque las pruebas se han hecho usando zonas de numpy, que empiezan desde arriba. Si despues se usa las coordenadas del CCD, que empiezan desde abajo, habra que cambiar la y1 por y2
 
-print(center)
-radius = 1024
+radius = 809 # con 810 tiene un pixel de borde
 mask = create_circular_mask(h, w, center=center, radius=radius)
-masked_img = b.copy()
+masked_img = image_data.copy()
 
 masked_img[~mask] = 0
-x = ma.masked_array(b, ~mask)
+x = ma.masked_array(image_data, ~mask)
 mediana = ma.median(x)
-print(mediana)
 
-#print('mask')
-#print(mask)
-#print('x')
-#print(x)
-#plt.figure()
-#plt.imshow(a)
+mostrarresultados(['ccdsec','h', 'w', 'center_real', 'center', 'radius', 'mediana'],
+                  [ccdsec, h, w, center_real, center, radius, mediana])
 
 
 plt.figure()
-plt.imshow(masked_img)
-plt.title('masked_img')
+plt.imshow(masked_img, origin='low')
+plt.title('masked_img - array[~mask]')
 
 plt.figure()
-plt.imshow(x)
-plt.title('x')
+plt.imshow(x, origin='low')
+plt.title('x - ma.masked_array(array, ~mask)')
 
 plt.figure()
-plt.imshow(mask)
-plt.title('mask')
+plt.imshow(mask, origin='low')
+plt.title('mask - create_circular_mask(h,w,center,radius)')
 
 plt.show()
